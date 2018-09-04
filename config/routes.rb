35b7599881fb599f90e81cab6fcfcb2cc811ebc6
Jordan Rand <jorrand@gmail.com>
2018-09-04 12:17:11 -0600

    added topics model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'subs#index'
 
-  resources :subs
+  resources :subs do
+    resources :topics
+  end
 
   # get '/subs', to: 'subs#index'
   # get '/subs/:id', to: 'subs#show'
