f68f0c3c51c61f8025d31f6f927cf3664f649ec3
Jordan Rand <jorrand@gmail.com>
2018-09-04 14:58:51 -0600

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
     resources :topics
   end
 
+  resources :topics do
+    resources :comments
+  end
+
+  scope 'topics/:topic_id', ad: 'topic' do
+    resources :commetns, only: [:new, :create]
+  end
+
   # get '/subs', to: 'subs#index'
   # get '/subs/:id', to: 'subs#show'
   # delete '/subs/:id', to: 'subs#destroy'
